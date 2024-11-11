3f8a9b6bf0485adef2829f9b10dddff9509285f0
AnishPun <ANISH.PUN@metropolia.fi>
2024-11-11 19:18:22 +0200

    Backend setup for favoritecity and functioning as expected (insert, update and delete)

--- app/src/main/java/com/example/weathermate/MainActivity.kt
+++ app/src/main/java/com/example/weathermate/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.weathermate
 
-
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,23 +17,44 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewModelScope
+import com.example.weathermate.model.FavoriteCity
 import com.example.weathermate.navigation.WeatherNavigation
+import com.example.weathermate.screens.favorites.FavoriteCityViewModel
 import com.example.weathermate.ui.theme.WeatherMateTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @ExperimentalComposeUiApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val favoriteCityViewModel: FavoriteCityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             WeatherMateApp()
         }
+        /*
+        // Trigger operations sequentially
+        favoriteCityViewModel.viewModelScope.launch {
+
+            val city = FavoriteCity(city = "Helsinki", country = "Finland")
+            favoriteCityViewModel.insertFavorite(city)
+            delay(1000) // Wait for 1 second to simulate time between operations
+
+            val updatedCity = FavoriteCity(city = "Helsinki", country = "Updated Finland")
+            favoriteCityViewModel.updateFavorite(updatedCity)
+            delay(1000)
+
+            favoriteCityViewModel.deleteFavorite(updatedCity)
+        }
+            **/
     }
 }
-
 
 @Composable
 fun WeatherMateApp() {
---
+++ app/src/main/java/com/example/weathermate/screens/favorites/FavoriteCityViewModel.kt
@@ -0,0 +1,67 @@
+package com.example.weathermate.screens.favorites
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weathermate.model.FavoriteCity
+import com.example.weathermate.repository.WeatherDbRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+// ViewModel class that interacts with the repository to manage the favorite cities
+@HiltViewModel
+class FavoriteCityViewModel @Inject constructor(private val repository: WeatherDbRepository) : ViewModel(){
+    // Mutable state flow to hold the list of favorite cities
+    private val _favoriteList = MutableStateFlow<List<FavoriteCity>>(emptyList())
+    // Immutable state flow to expose the list of favorite cities
+    val favoriteList = _favoriteList.asStateFlow()
+
+    // Initialize the favorite cities list
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            // Collect the list of favorite cities from the repository
+            repository.getFavoriteCities().distinctUntilChanged()
+                .collect { listOfFavoriteCities ->
+                    if (listOfFavoriteCities.isNotEmpty()) {
+                        // Message if the list of favorite cities is empty
+                        Log.d("TAG", ": Empty Favorites")
+
+                    } else {
+                        // Update the list of favorite cities if not empty
+                        _favoriteList.value = listOfFavoriteCities
+                        Log.d("Favorites", ": ${favoriteList.value}")
+                    }
+                }
+        }
+    }
+
+    // Function to insert a favorite city
+    fun insertFavorite(city: FavoriteCity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertFavoriteCity(city)
+            Log.d("DatabaseOperation", "Inserted city: ${city.city}")
+        }
+    }
+
+    // Function to update a favorite city
+    fun updateFavorite(city: FavoriteCity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateFavoriteCity(city)
+            Log.d("DatabaseOperation", "Updated city: ${city.city}")
+        }
+    }
+
+    // Function to delete a favorite city
+    fun deleteFavorite(city: FavoriteCity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteFavoriteCity(city)
+            Log.d("DatabaseOperation", "Deleted city: ${city.city}")
+        }
+    }
+
+}
